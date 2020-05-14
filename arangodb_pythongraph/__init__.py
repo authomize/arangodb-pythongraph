09a9adceb3be0a181d3a08a03a1575e6b21daae8
Avi <avi.aminov@authomize.com>
2020-05-14 17:33:06 +0300

    exposed an internal function that transforms the response itself to a python graph

--- arangodb_pythongraph/__init__.py
+++ arangodb_pythongraph/__init__.py
@@ -1,54 +1,27 @@
-from typing import Dict
+from typing import Dict, Iterable
 
 from arango.aql import AQL
 from arango.database import StandardDatabase
 from pyintergraph import InterGraph
 
+DEFAULT_LABEL = '_id'
 
-def _extract_graph_from_graph_response(res: Dict):
-    vertices = {v['_id']: v for v in res['vertices']}
-    edges = {(e['_from'], e['_to']): e for e in res['edges']}
+VERTICES = 'vertices'
+EDGES = 'edges'
+
+
+def _split_graph_obj_to_vertices_edges(res: Dict):
+    vertices = {v[DEFAULT_LABEL]: v for v in res.get(VERTICES, [])}
+    edges = {(e['_from'], e['_to']): e for e in res.get(EDGES, [])}
     return vertices, edges
 
 
-def execute_to_pygraph(db: StandardDatabase, query: str, label='_id', **kwargs):
-    """Run a query and return a PythonGraph.
-
-    :param db: ArangoDB connection.
-    :type query: arango.database.StandardDatabase
-    :param query: Query to execute.
-    :type query: str
-    :param **kwargs: Additional arguments for db.aql.execute (e.g. bind_vars,
-    :type opt_rules: list
-
-    :return: Python graph representation
-    :rtype: InterGraph
-    :raise ValueError: If the query result is not a graph.
-    """
-    return _execute_to_pythongraph(db.aql, query, label, **kwargs)
-
-
-def _execute_to_pythongraph(aql: AQL, query: str, label='_id', **kwargs):
-    """Run a query and return a PythonGraph.
-
-    :param aql: AQL instance.
-    :type query: arango.database.StandardDatabase
-    :param query: Query to execute.
-    :type query: str
-    :param **kwargs: Additional arguments for db.aql.execute (e.g. bind_vars,
-    :type opt_rules: list
-
-    :return: Python graph representation
-    :rtype: InterGraph
-    :raise ValueError: If the query result is not a graph.
-    """
-    cur = aql.execute(query, **kwargs)
-
+def get_graph_from_path_collection(cur: Iterable, label=DEFAULT_LABEL) -> InterGraph:
     nodes_dict, edges_dict = dict(), dict()
 
     for res in cur:
-        if 'vertices' in res and 'edges' in res:  #
-            new_vertices, new_edges = _extract_graph_from_graph_response(res)
+        if VERTICES in res:  #
+            new_vertices, new_edges = _split_graph_obj_to_vertices_edges(res)
             nodes_dict.update(new_vertices)
             edges_dict.update(new_edges)
         else:
@@ -68,6 +41,41 @@ def _execute_to_pythongraph(aql: AQL, query: str, label='_id', **kwargs):
     is_directed = True  # It seems that Arango supports only directed graphs
 
     return InterGraph(nodes, node_labels, node_attributes, edges, edge_attributes, is_directed)
+
+
+def execute_to_pygraph(db: StandardDatabase, query: str, label=DEFAULT_LABEL, **kwargs):
+    """Run a query and return a PythonGraph.
+
+    :param db: ArangoDB connection.
+    :type query: arango.database.StandardDatabase
+    :param query: Query to execute.
+    :type query: str
+    :param **kwargs: Additional arguments for db.aql.execute (e.g. bind_vars,
+    :type opt_rules: list
+
+    :return: Python graph representation
+    :rtype: InterGraph
+    :raise ValueError: If the query result is not a graph.
+    """
+    return _execute_to_pythongraph(db.aql, query, label, **kwargs)
+
+
+def _execute_to_pythongraph(aql: AQL, query: str, label=DEFAULT_LABEL, **kwargs):
+    """Run a query and return a PythonGraph.
+
+    :param aql: AQL instance.
+    :type query: arango.database.StandardDatabase
+    :param query: Query to execute.
+    :type query: str
+    :param **kwargs: Additional arguments for db.aql.execute (e.g. bind_vars,
+    :type opt_rules: list
+
+    :return: Python graph representation
+    :rtype: InterGraph
+    :raise ValueError: If the query result is not a graph.
+    """
+    cur = aql.execute(query, **kwargs)
+    return get_graph_from_path_collection(cur, label=label)
 
 
 def register():
